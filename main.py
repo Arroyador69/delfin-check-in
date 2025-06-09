833deed8626af873806d7b1a18316cea870ad00f
Arroyador69 <143028288+Arroyador69@users.noreply.github.com>
2025-06-10 00:36:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,4 @@ def generar_web():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port) 
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+   
